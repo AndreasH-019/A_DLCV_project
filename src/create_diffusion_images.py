62f1da87454128d08f1ea98c4f5543e94a070de0
Christian <s194255@student.dtu.dk>
2023-11-19 14:05:31 +0100

    Update create_diffusion_images.py

--- src/create_diffusion_images.py
+++ src/create_diffusion_images.py
@@ -10,37 +10,29 @@ def parserargs():
     args = parser.parse_args()
     return args
 
-def get_prompts():
-    # prompts = ["photo realistic giraffe on white background",
-    #            "photo realistic giraffe on white background looking to the side",
-    #            "photo realistic giraffe on white background running",
-    #            "photo realistic giraffe on white background sleeping",
-    #            "cartoon giraffe on white background"]
-    prompts = ["giraffe on white background"]
+def get_prompts(class_):
+    prompts = [f"{class_} on white background",
+               f"full body {class_} on white background"]
     return prompts
 
-
-def generate_images(pipe, prompt, debug, save_root, promp_idx):
-    num_inference_steps = 1 if debug else 100
-    num_images_per_prompt = 1 if debug else 32
-    images = pipe(prompt, num_inference_steps=num_inference_steps,
-                  num_images_per_prompt=num_images_per_prompt).images
-    for j, image in enumerate(images):
-        save_path = os.path.join(save_root, f"promp_{promp_idx}_images_{j}.jpg")
-        image.save(save_path)
-
-def get_save_root():
-    save_root = "../../generated_images"
-    if os.path.exists(save_root):
-        shutil.rmtree(save_root)
-    os.mkdir(save_root)
-    return save_root
+def get_class_save_roots(classes):
+    main_root = "../../generated_images"
+    if os.path.exists(main_root):
+        shutil.rmtree(main_root)
+    os.mkdir(main_root)
+    class_save_roots = []
+    for class_ in classes:
+        save_root = os.path.join(main_root, class_)
+        os.mkdir(save_root)
+        class_save_roots.append(save_root)
+    return main_root, class_save_roots
 
 def zip_images(save_root):
     shutil.make_archive(save_root, "zip", save_root)
 
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    classes = ['giraffe', 'elephant']
     args = parserargs()
     dtype = torch.float32 if args.debug else torch.float16
     num_imgs_to_generate = 1 if args.debug else 200
@@ -49,25 +41,23 @@ if __name__ == "__main__":
     pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=dtype,
                                                    requires_safety_checker=False, safety_checker=None)
     pipe.to(device)
-    prompts = get_prompts()
-    save_root = get_save_root()
-    prompt_idx = 0
-    num_generated_imgs = 0
-    i = 0
-    while num_generated_imgs <= num_imgs_to_generate:
-        if prompt_idx >= len(prompts):
-            prompt_idx = 0
-        prompt = prompts[prompt_idx]
-        images = pipe(prompt, num_inference_steps=num_inference_steps,
-                      num_images_per_prompt=num_images_per_prompt).images
-        for j, image in enumerate(images):
-            save_path = os.path.join(save_root, f"step_{i}_image_{j}.jpg")
-            image.save(save_path)
-        prompt_idx += 1
-        i += 1
-        num_generated_imgs += num_images_per_prompt
-    zip_images(save_root)
-    # for prompt_idx, prompt in enumerate(prompts):
-    #     generate_images(pipe, prompt, args.debug, save_root, prompt_idx)
-    # zip_images(save_root)
+    main_root, class_save_roots = get_class_save_roots(classes)
+    for class_, class_save_root in zip(classes, class_save_roots):
+        prompts = get_prompts(class_)
+        prompt_idx = 0
+        num_generated_imgs = 0
+        i = 0
+        while num_generated_imgs <= num_imgs_to_generate:
+            if prompt_idx >= len(prompts):
+                prompt_idx = 0
+            prompt = prompts[prompt_idx]
+            images = pipe(prompt, num_inference_steps=num_inference_steps,
+                          num_images_per_prompt=num_images_per_prompt).images
+            for j, image in enumerate(images):
+                save_path = os.path.join(class_save_root, f"step_{i}_image_{j}.jpg")
+                image.save(save_path)
+            prompt_idx += 1
+            i += 1
+            num_generated_imgs += num_images_per_prompt
+    zip_images(main_root)
 
